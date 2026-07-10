a8009d0fe8b005bd740d1d1fae292f99b1c01b0a
Rajkamal Yadav <143881829+Rajkamalout@users.noreply.github.com>
2026-07-10 12:58:29 +0530

    Create Bulid A Movie Ticket Booking.py

---
+++ Bulid A Movie Ticket Booking.py
@@ -0,0 +1,17 @@
+# Step 4
+# Now you will check whether the user is allowed to book an evening show based on their age.
+
+# Create an if statement to check if age is greater than or equal to 21. Inside the body of the if statement, print User is eligible for Evening shows.
+
+base_price = 15
+age = 21
+seat_type = 'Gold'
+show_time = 'Evening'
+
+if age>=22:
+    print('User is eligible for Evening shows')
+if age>=21:
+    print('User is eligible for Evening shows')
+
+
+step5:
